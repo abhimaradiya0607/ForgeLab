cd788e41545d04abe4b03eb560e1272e8520c0ea
Abhi Maradiya <abhimaradiya63@gmail.com>
2026-09-05 12:56:42 +0530

    added: addRepo Logic in controllers

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,4 +1,15 @@
-export async function addRepo(params) {
-    console.log("Added files to the repoo.");
-    
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+export async function addRepo(filePath) {
+    const repopath=path.resolve(process.cwd(),".ForgeLab");
+    const stagingPath=path.resolve(repopath,"staging");
+    try {
+        await fs.mkdir(stagingPath,{recursive:true});
+        const fileName=path.basename(filePath);
+        await fs.copyFile(filePath,path.join(stagingPath,fileName));
+        console.log(`File ${fileName} added to the staging area!`);
+    } catch (err) {
+        console.error("Error adding file ",err);
+    }
 }
